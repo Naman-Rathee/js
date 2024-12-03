91589fb3a1e379c2132cea557e4b2e8b7969c307
Naman-Rathee <namanrathee@outlook.com>
2024-12-03 19:11:05 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "naman@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "212121"
+accountCity = "Gurgaon"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
